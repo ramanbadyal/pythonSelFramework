7b9f89d1d4812480c440ccab4c9fc6fce562969c
ramanbadyal <55278267+ramanbadyal@users.noreply.github.com>
2021-04-14 23:04:09 -0400

    Update checkoutPage.py

--- pageObjects/checkoutPage.py
+++ pageObjects/checkoutPage.py
@@ -26,8 +26,7 @@ class CheckoutPage:
         self.driver.find_element(*CheckoutPage.finalButton).click()
         confirmPage = ConfirmPage(self.driver)
         return confirmPage
-
-
+    # this is my third change.
 
 
 
